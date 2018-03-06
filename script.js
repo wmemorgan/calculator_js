9ec6760ca8f3baf5d79a9ced53eb14b6b0e42ef4
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-03-06 23:25:07 +0800

    Add key press display to screen box

--- script.js
+++ script.js
@@ -2,38 +2,19 @@ let screen = document.getElementsByClassName("screen");
 let key = document.getElementsByClassName("key");
 let zeroKey = document.getElementById("zerokey");
 
-
 const displayKey = (i) => {
-  return () => { console.log("you clicked on key " + key[i].innerText);}
+  return () => { 
+    console.log("you clicked on key " + key[i].innerText);
+    screen[0].innerText = key[i].innerText;
+  }
 };
 
-const addItemFeatures = (key) => {
-  displayKey(key);
+const addItemFeatures = (i) => {
+  displayKey(i);
 }
 
-// for (i in key) {
-//   function(index) {
-//     imageArray[index].addEventListener("click", function () {
-//       console.log("You clicked key: ", index);
-//         });
-//   } (i);
-// }
-
-// for (var i = 0; i < key.length; i++) {
-//   key[i].addEventListener("click", bindClick(i));
-// }
-
-for (var i = 0; i < key.length; i++) {
+for (let i = 0; i < key.length; i++) {
   key[i].addEventListener("click", displayKey(i));
 }
 
-function bindClick(i) {
-  return function () {
-    console.log("you clicked on key " + key[i].innerText);
-  };
-}
 
-key[4].addEventListener("click", function () { console.log(key[4].innerText) });
-
-
-// zeroKey.forEach(addItemFeatures);
