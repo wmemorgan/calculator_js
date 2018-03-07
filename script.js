4581c0ac61f9a7c26fe6296286ea845ceb5490c8
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-03-07 16:00:54 +0800

    Add arithmetic functions

--- script.js
+++ script.js
@@ -3,24 +3,42 @@ let display = document.getElementById("display");
 let equation = document.getElementById("equation");
 let key = document.getElementsByClassName("key");
 let zeroKey = document.getElementById("zerokey");
+let equals = document.getElementById("equals");
 
 const displayKey = (i) => {
   return () => { 
     console.log("you clicked on key " + key[i].innerText);
-    let keyInput = document.createTextNode(key[i].innerText);
     display.innerText = key[i].innerText;
+    let keyInput = document.createTextNode(key[i].innerText);
     equation.appendChild(keyInput); 
-    // screen[0].innerText = key[i].innerText;
-
   }
 };
 
-const addItemFeatures = (i) => {
-  displayKey(i);
+const calculate = () => {
+  return () => {
+    let eq = equation.innerText;
+    eq = eq.replace(/×/, "*");
+    eq = eq.replace(/÷/, "/");
+    eq = eq.split('');
+    eq.splice(-1, 1);
+    eq = eq.join('');
+    answer = eval(eq);
+    console.log("This equation is:", eq);
+    console.log("The answer is:", answer);
+    display.innerText = answer;
+    let keyInput = document.createTextNode(answer);
+    equation.appendChild(keyInput); 
+  }
 }
+
+// const addItemFeatures = (i) => {
+//   displayKey(i);
+// }
 
 for (let i = 0; i < key.length; i++) {
   key[i].addEventListener("click", displayKey(i));
 }
+
+equals.addEventListener("click", calculate());
 
 
